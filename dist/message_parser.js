84aabb1b7669c444995ecc1aa74ba4a3f1fd5a90
Miles Zimmerman <miles@maztek.io>
2016-05-28 02:40:17 -0700

    Adding message parsing and other goodies

---
+++ dist/message_parser.js
@@ -0,0 +1,171 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var OXIDE_REGEX = /^\[Oxide\]\s(\d{1,2}:\d{2})\s\[(\w+)\]\s(.*)$/;
+var RCON_REGEX = /^\[RCON\]\[(.+)\]\s(.+)$/;
+var CHAT_REGEX = /^\[CHAT\]\s(.+)\[(\d+)\/(\d+)\]\s:\s(.*)$/;
+
+var SERVER_DESCRIPTION_REGEX = /^server\.description:\s\"(.*)\"$/;
+var SERVER_URL_REGEX = /^server\.url:\s\"(.*)\"$/;
+var SERVER_IMAGE_REGEX = /^server\.headerimage:\s\"(.*)\"$/;
+var SERVER_HOSTNAME_REGEX = /^server\.hostname:\s\"(.*)\"$/;
+
+var PLAYER_STATUS_REGEX = /^(\d+)\s"(.+)"\s+(\d{1,10})\s+(\w+)\s+([\w\.\:]+)\s+/;
+
+/**
+ * Parse RCON messages into constituent pieces
+ */
+
+var MessageParser = function () {
+  function MessageParser() {
+    _classCallCheck(this, MessageParser);
+
+    throw new Error('MessageParser class should not be instantiated.');
+  }
+
+  // Entrypoint
+
+
+  _createClass(MessageParser, null, [{
+    key: 'parse',
+    value: function parse(message) {
+      if (MessageParser.isOxide(message)) {
+        return MessageParser._toOxide(message);
+      }
+      if (MessageParser.isRCON(message)) {
+        return MessageParser._toRCON(message);
+      }
+      if (MessageParser.isChat(message)) {
+        return MessageParser._toChat(message);
+      }
+      return MessageParser._toGeneral(message);
+    }
+
+    /**
+     * Checkers
+     */
+
+  }, {
+    key: 'isOxide',
+    value: function isOxide(message) {
+      return OXIDE_REGEX.test(message);
+    }
+  }, {
+    key: 'isRCON',
+    value: function isRCON(message) {
+      return RCON_REGEX.test(message);
+    }
+  }, {
+    key: 'isChat',
+    value: function isChat(message) {
+      return CHAT_REGEX.test(message);
+    }
+  }, {
+    key: 'isGeneral',
+    value: function isGeneral(message) {
+      return !MessageParser.isOxide(message) && !MessageParser.isRCON(message) && !MessageParser.isChat(message);
+    }
+
+    /**
+     * Specific Parsers
+     */
+
+  }, {
+    key: 'parseServerDescription',
+    value: function parseServerDescription(message) {
+      var props = SERVER_DESCRIPTION_REGEX.exec(message);
+      return props[1];
+    }
+  }, {
+    key: 'parseServerURL',
+    value: function parseServerURL(message) {
+      var props = SERVER_URL_REGEX.exec(message);
+      return props[1];
+    }
+  }, {
+    key: 'parseServerImage',
+    value: function parseServerImage(message) {
+      var props = SERVER_IMAGE_REGEX.exec(message);
+      return props[1];
+    }
+  }, {
+    key: 'parseServerHostname',
+    value: function parseServerHostname(message) {
+      var props = SERVER_HOSTNAME_REGEX.exec(message);
+      return props[1];
+    }
+  }, {
+    key: 'parserPlayerStatus',
+    value: function parserPlayerStatus(message) {
+      message = message + "";
+      return message.split('\n').slice(6, -1).map(function (player) {
+        var p = PLAYER_STATUS_REGEX.exec(player);
+        return { steamId: p[1], name: p[2], ping: p[3], connected: p[4], ipAddress: p[5] };
+      });
+    }
+
+    /**
+     * General Parsers
+     */
+
+  }, {
+    key: '_toOxide',
+    value: function _toOxide(message) {
+      var props = OXIDE_REGEX.exec(message);
+      return {
+        type: 'oxide',
+        msg: message,
+        timestamp: new Date(),
+        level: props[2].toLowerCase(),
+        content: props[3]
+      };
+    }
+  }, {
+    key: '_toRCON',
+    value: function _toRCON(message) {
+      var props = RCON_REGEX.exec(message);
+      return {
+        type: 'rcon',
+        msg: message,
+        timestamp: new Date(),
+        host: props[1],
+        content: props[2]
+      };
+    }
+  }, {
+    key: '_toChat',
+    value: function _toChat(message) {
+      var props = CHAT_REGEX.exec(message);
+      return {
+        type: 'chat',
+        msg: message,
+        timestamp: new Date(),
+        name: props[1],
+        id: props[2],
+        steamId: props[3],
+        content: props[4]
+      };
+    }
+  }, {
+    key: '_toGeneral',
+    value: function _toGeneral(message) {
+      return {
+        type: 'general',
+        msg: message,
+        timestamp: new Date(),
+        content: message
+      };
+    }
+  }]);
+
+  return MessageParser;
+}();
+
+exports.default = MessageParser;
--- dist/rustcon.js
+++ dist/rustcon.js
@@ -4,16 +4,231 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _sourcecon = require('sourcecon');
+
+var _sourcecon2 = _interopRequireDefault(_sourcecon);
+
+var _events = require('events');
+
+var _message_parser = require('./message_parser');
+
+var _message_parser2 = _interopRequireDefault(_message_parser);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var SERVER_UPDATE_DELAY = 500;
 
 /**
  * rustcon
  */
 
-var RustCon = function RustCon() {
-  _classCallCheck(this, RustCon);
+var RustCon = function (_EventEmitter) {
+  _inherits(RustCon, _EventEmitter);
 
-  console.log('test');
-};
+  function RustCon(ip, port, pass) {
+    _classCallCheck(this, RustCon);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(RustCon).call(this));
+
+    if (!ip) {
+      throw new Error('Required IP address param missing.');
+    }
+    if (!port) {
+      throw new Error('Required port param missing.');
+    }
+    if (!pass) {
+      throw new Error('Required rcon password param missing.');
+    }
+
+    // Properties
+    _this.ip = ip;
+    _this.port = port;
+    _this.pass = pass;
+    _this.hostname = null;
+    _this.description = null;
+    _this.url = null;
+    _this.image = null;
+    _this.players = [];
+
+    _this.connected = false;
+
+    // Private
+    _this._sourcecon = new _sourcecon2.default(ip, port);
+    return _this;
+  }
+
+  _createClass(RustCon, [{
+    key: 'connect',
+    value: function connect() {
+      var _this2 = this;
+
+      this._connect().then(function () {
+        return _this2._auth();
+      }).then(function () {
+        return _this2.updateServer();
+      }).then(function () {
+        return _this2.updatePlayers();
+      }).then(function () {
+        return _this2._listen();
+      }).catch(function (error) {
+        console.error(error);
+        process.exit();
+      });
+    }
+
+    /**
+     * Fetch server info
+     */
+
+  }, {
+    key: 'updateServer',
+    value: function updateServer() {
+      var _this3 = this;
+
+      return this.getHostname().then(function () {
+        return _this3.getDescription();
+      }).then(function () {
+        return _this3.getURL();
+      }).then(function () {
+        return _this3.getImage();
+      }).then(function () {
+        var info = { hostname: _this3.hostname, description: _this3.description, url: _this3.url, image: _this3.image };
+        _this3.emit('server_info', info);
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+
+    /**
+     * Fetch server info
+     */
+
+  }, {
+    key: 'updatePlayers',
+    value: function updatePlayers() {
+      var _this4 = this;
+
+      return this.getPlayers().then(function () {
+        _this4.emit('player_status', _this4.players);
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+  }, {
+    key: 'getHostname',
+    value: function getHostname() {
+      return this._getServerAttribute('server.hostname', 'hostname', 'parseServerHostname');
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this._getServerAttribute('server.description', 'description', 'parseServerDescription');
+    }
+  }, {
+    key: 'getURL',
+    value: function getURL() {
+      return this._getServerAttribute('server.url', 'url', 'parseServerURL');
+    }
+  }, {
+    key: 'getImage',
+    value: function getImage() {
+      return this._getServerAttribute('server.headerimage', 'image', 'parseServerImage');
+    }
+  }, {
+    key: 'getPlayers',
+    value: function getPlayers() {
+      return this._getServerAttribute('status', 'players', 'parserPlayerStatus');
+    }
+
+    /**
+     * Handle updating server attr
+     * @private
+     */
+
+  }, {
+    key: '_getServerAttribute',
+    value: function _getServerAttribute(cmd, attr, func) {
+      var _this5 = this;
+
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          _this5._sourcecon.send(cmd, function (error, res) {
+            if (error) {
+              reject(error);
+            }
+            _this5[attr] = _message_parser2.default[func](res);
+            resolve();
+          });
+        }, SERVER_UPDATE_DELAY);
+      });
+    }
+
+    /**
+     * Connect
+     * @private
+     */
+
+  }, {
+    key: '_connect',
+    value: function _connect() {
+      var _this6 = this;
+
+      return new Promise(function (resolve, reject) {
+        _this6._sourcecon.connect(function (error) {
+          if (error) {
+            reject(error);
+          }
+          resolve(_this6.sourcecon);
+        });
+      });
+    }
+
+    /**
+     * Auth
+     * @private
+     */
+
+  }, {
+    key: '_auth',
+    value: function _auth() {
+      var _this7 = this;
+
+      return new Promise(function (resolve, reject) {
+        _this7._sourcecon.auth(_this7.pass, function (error) {
+          if (error) {
+            reject(error);
+          }
+          _this7.connected = true;
+          resolve(_this7.sourcecon);
+        });
+      });
+    }
+
+    /**
+     * Listen for messages
+     * @private
+     */
+
+  }, {
+    key: '_listen',
+    value: function _listen() {
+      var _this8 = this;
+
+      this._sourcecon.on('message', function (msg) {
+        _this8.emit('message', _message_parser2.default.parse(msg.body + ""));
+      });
+    }
+  }]);
+
+  return RustCon;
+}(_events.EventEmitter);
 
 exports.default = RustCon;
