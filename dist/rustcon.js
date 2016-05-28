28fdefb0de70534ed6a839fb4a13a4773775eb2b
Miles Zimmerman <miles@maztek.io>
2016-05-27 17:20:15 -0700

    Scaffolding

---
+++ dist/rustcon.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * rustcon
+ */
+
+var RustCon = function RustCon() {
+  _classCallCheck(this, RustCon);
+
+  console.log('test');
+};
+
+exports.default = RustCon;
